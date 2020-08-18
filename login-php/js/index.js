dbf82e9eb700a73f315d9f9ac2b8baf578f6e20d
Gabriel <gab24wal@gmail.com>
2020-08-18 16:20:36 -0300

    login

--- login-php/js/index.js
+++ login-php/js/index.js
@@ -1,13 +1,17 @@
 $(document).ready( function(){
-    $.ajax({
-        type: 'POST',
-        url: 'cargar_listas.php', /* Consulta en la base de datos donde extrae, procesa y devuelve el resultado en html */
-        data: {'peticion': 'cargas_listas'}
-    })
-    .done( function( lista_rep ){
-        alert('se consiguieron resultados')
-    })
-    .fail(function(){
-        alert('Error al cargar la lista de reproduccion')
-    })
+    recargaLista();
+    $('#gd').change( function(){
+       recargaLista(); 
+    });
 })
+
+function recargaLista(){
+    $.ajax({
+        type: "POST",
+        url: "cargar_lista.php",
+        data: "gd= " + $('#gd').text(),
+        success:function(r) {
+            $('#ncap').html(r);
+        } 
+    })    
+}
--- login-php/selection.php
+++ login-php/selection.php
@@ -27,12 +27,50 @@
         <br> Bienvenido!
         <br> <?= $user['nombre'] ?> <?= $user['apellido']?>
         <br> Sigamos trabajando! 
-        <script type="text/javascript" src="js/index.js"></script>
         <br>
-        <a href="logout.php"> Salir</a>
     <?php endif;?>
 </head>
 <body>
-    
+<label> Elija un guemes documentado:</label>
+        <select id = 'GD' name = "GD">
+            <option value = gd1> 1 </option>
+            <option value = gd2> 2 </option>
+            <option value = gd3> 3 </option>
+            <option value = gd4> 4 </option>
+            <option value = gd5> 5 </option>
+            <option value = gd6> 6 </option>
+            <option value = gd7> 7 </option>
+            <option value = gd8> 8 </option>
+            <option value = gd9> 9 </option>
+            <option value = gd10> 10 </option>
+            <option value = gd11> 11 </option>
+            <option value = gd12> 12 </option>
+        </select>
+        <!--<script type="text/javascript" src="js/index.js"> </script>-->
+        <br>
+        <div id='ncap'> </div>
+        <a href="logout.php"> Salir</a>
 </body>
 </html>
+
+<script type = "text/javascript">
+    $(document).ready(function(){
+        recargarLista();
+        $('#GD').change(function(){
+            recargarLista();
+        });
+    })
+</script>
+
+<script type = "text/javascript">
+    function recargarLista(){
+        $.ajax({
+            type: "POST",
+            url: "carga.php",
+            data: "guemes=" + $('#GD').val(),
+            success:function(r){
+                $('#ncap').html(r)
+            }
+        });
+    }
+</script>
