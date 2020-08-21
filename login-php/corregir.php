7b7f7736f1f1b59d409c76e7ac60ab1c79f101c5
analidia27 <anamechaca27@gmail.com>
2020-08-20 23:29:34 -0300

    Copy and Paste

--- login-php/corregir.php
+++ login-php/corregir.php
@@ -63,27 +63,28 @@
         
         <form id = form method = "POST " action=""> 
             <label for=''>Capítulo: </label>  
-            <input type="text" name ='ncap' id ='ncap' value=" <?php  echo $ncap; ?> ">
+            <input type="text" name ='ncap' id ='ncap' disabled='true' value=" <?php  echo $ncap; ?> ">
             <br>
             <label for=''>Página: </label>
-            <input type="text" name ='npag' id ='npag' value="<?php  echo $npag; ?>">
+            <input type="text" name ='npag' id ='npag' disabled='true' value="<?php  echo $npag; ?>">
             <br>
             <label for=''>Número de línea: </label>
-            <input type="text" name ='nlinea' id ='nlinea' value="<?php  echo $nlinea; ?>">
+            <input type="text" name ='nlinea' id ='nlinea' disabled='true' value="<?php  echo $nlinea; ?>">
             <br>
 
             <label for=''>Línea original: </label> 
-            <textarea name='tocr' disabled='true'> "<?php  echo $tocr; ?>"</textarea>
-            <input class = "copiarTocr" type="button" id="copiarTocr" value="Copiar" />
-            
+            <textarea name='tocr' id="tocr" disabled ='true'> "<?php  echo $tocr; ?>"</textarea>
+            <input class = "copiar" type="button" id="copiarTocr" value="Copiar para editar" onclick="copy()" />
             <br>
+
             <label for=''>Línea sugerida: </label> <br>
-            <textarea name='tsug' disabled='true'> "<?php  echo $tocr; ?>"</textarea>
-            <input class = "copiarTsug" type="button" id="copiarTsug" value="Copiar" />
+            <textarea name='tsug' id="tsug" disabled='true'> "<?php  echo $tocr; ?>"</textarea>
+            <input class = "copiar" type="button" id="copiarTsug" value="Copiar para editar" onclick="copy2()" />
             <br>
 
             <label for=''>Línea a corregir : </label>
-            <textarea name='tcorrg', id='tcorrg'> </textarea> 
+            <textarea name='tcorrg' id="tcorrg" > </textarea>
+
             <br>
             <input type='submit', name='siguiente', id='siguiente',value='Siguiente'>
          </form>
@@ -93,46 +94,18 @@
         <a href="seleccion.php"> Volver</a>
         <a href="logout.php"> Salir</a>
     <?php endif;?>
-
+    
     <script type="text/javascript">
-        var button = document.getElementById("copiarTocr"),
-            input = document.getElementById("tocr");
-
-        button.addEventListener("click", function(event) {
-            event.preventDefault();lo
-            input.select();
-            document.execCommand("copy");
-        });
+        function copy() {
+            $("#tcorrg").val(document.getElementById("tocr").value);
+        }
+        function copy2() {
+            $("#tcorrg").val(document.getElementById("tsug").value); 
+        }
     </script>
+
     
 </body>
 </html>
 
-<script type = "text/javascript">
-    $(document).ready(function(){
-        recargarLista();
-        $('#siguiente').click(function(){
-            recargarLista();
-        });
-    })
-</script>
 
-<script type = "text/javascript">
-    function recargarLista(){
-
-        $.ajax({
-            type: "POST",
-            url: "update.php",
-            //url: $(this).attr('action'),
-            data: $(this).serialize(),
-            //data: { "ncap": $('#ncap').val(), "npag":$('#npag').val(), "nlinea":$('#nlinea').val()},
-            success:function(r){
-               // $('#list').html(r)
-               alert('AJAX call was successful!');
-            },
-            error: function() {
-                alert('There was some error performing the AJAX call!');
-            }
-        });
-    }
-</script>
