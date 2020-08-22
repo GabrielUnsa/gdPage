aaab9a28663d5589cf0f512194836d8057068aba
Gabriel <gab24wal@gmail.com>
2020-08-22 20:15:42 -0300

    login

--- login-php/corregir.php
+++ login-php/corregir.php
@@ -3,7 +3,7 @@
     require 'database.php';
 
     if( isset($_SESSION['user_id']) ){
-        $records = $conn->prepare('SELECT idusr, nickname, nombre, apellido, password FROM users WHERE idusr = :idusr');
+        $records = $conn->prepare('SELECT idusr FROM users WHERE idusr = :idusr');
         $records->bindParam(':idusr', $_SESSION['user_id']);
         $records->execute();
         $results = $records->fetch();
@@ -23,53 +23,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/style.css" rel="stylesheet">
-    <title>Seleccion GD</title>
+    <title>Correcion GD</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body> 
     <?php require 'partials/header.php' ?>
     <?php if( !empty( $user ) ): ?>
-        <br> Bienvenido!
-        <br> <?= $user['nombre'] ?> <?= $user['apellido']?>
-        <br> Sigamos trabajando! 
-
         <div>
         <h1>Revisar Corrección</h1>
 
         <?php 
-        print_r($_POST);
-/*         
-        $ncap = $_POST['ncap'];
-        $npag = $_POST['npag'];
-        $nlinea = $_POST['nlinea'];
-        $guemes = 'gd1';
-        $sql = "SELECT tocr FROM {$guemes} WHERE 
-        ncap = :ncap AND npag = :npag AND nlinea = :nlinea" ;
-        $stmt = $conn->prepare($sql);
-        $stmt -> bindParam( ':ncap', $ncap );
-        $stmt -> bindParam( ':npag', $npag );
-        $stmt -> bindParam( ':nlinea', $nlinea );$
-        $resultado = $stmt->fetch();
-        $tcorrg = $resultado['tocr']; */
-        $records = $conn->prepare("SELECT * FROM gd1 WHERE idusr IS NULL LIMIT 1");
+        $records = $conn->prepare("SELECT ncap, npag, nline, tocr FROM gd1 WHERE idusr IS NULL LIMIT 1");
         $records->execute();
         $results = $records->fetch();
-        $ncap = $results['ncap'];
-        $npag = $results['npag'];
-        $nlinea = $results['nlinea'];
-        $tocr = $results['tocr'];  // print_r ($results); ?>
+        $ncap = $results[0];
+        $npag = $results[1];
+        $nlinea = $results[2];
+        $tocr = $results[3]; ?>
         
         
         <form id = "FORM" method = "POST" action="update.php"> 
+
             <label for=''>Capítulo: </label>  
-            <input type="text" name ='ncap' id ='ncap' disabled='true' value=" <?php  echo $ncap; ?> ">
+            <input type="text" name ='ncap' id ='ncap' readonly='true' value=" <?php  echo $ncap; ?> ">
             <br>
             <label for=''>Página: </label>
-            <input type="text" name ='npag' id ='npag' disabled='true' value="<?php  echo $npag; ?>">
+            <input type="text" name ='npag' id ='npag' readonly='true' value="<?php  echo $npag; ?>">
             <br>
             <label for=''>Número de línea: </label>
-            <input type="text" name ='nlinea' id ='nlinea' disabled='true' value="<?php  echo $nlinea; ?>">
+            <input type="text" name ='nlinea' id ='nlinea' readonly='true' value="<?php  echo $nlinea; ?>">
             <br>
 
             <label for=''>Línea original: </label> 
@@ -88,11 +71,10 @@
             <br>
             <input type='submit' name='siguiente' id='siguiente' value='Siguiente'>
          </form>
-        
-        <script type="text/javascript" src="js/index.js"></script>
         <br>
-        <a href="seleccion.php"> Volver</a>
-        <a href="logout.php"> Salir</a>
+        <a class = b2v href="seleccion.php"> Volver</a>
+        <a class = b3v href="logout.php"> Salir</a>
+        <br>
     <?php endif;?>
     
     <script type="text/javascript">
--- login-php/update.php
+++ login-php/update.php
@@ -1,48 +1,16 @@
 <?php
     session_start();
     require 'database.php';
-
-    if( isset($_SESSION['user_id']) ){
-        $records = $conn->prepare('SELECT idusr, nickname, nombre, apellido, password FROM users WHERE idusr = :idusr');
-        $records->bindParam(':idusr', $_SESSION['user_id']);
-        $records->execute();
-        $results = $records->fetch();
-        $user = null;
-        if( count($results) > 0 ){
-            $user = $results;
-        }
-    }
-    //update gd1 set tocr=' PAPELES QUE A LA MUERTE DE GÜEMES', tcorrg='hola' 
-    //where ncap=1 and npag = 11 and nlinea=1  fcorrg= 'SELECT CURRENT_TIMESTAMP();',
-    // if( isset($_POST['siguiente']) ){
-        //
-/*         $ncap = $_POST['ncap'];
-        $npag = $_POST['npag'];
-        $nlinea = $_POST['nlinea']; 
-        $tcorrg = $_POST['tcorrg']; */ 
-/*         $npag = 11;
-        $nlinea = 1; */
-        print_r($_POST);
-            if( !empty($tcorrg) ){
-                
-                $sql = "UPDATE gd1 SET tcorrg = :tcorrg,  idusr = :idusr WHERE ncap= :ncap and npag = :npag and nlinea= :nlinea";
-                $stmt = $conn -> prepare($sql);
-                $stmt -> bindParam( ':tcorrg',  $_POST['tcorrg'] );
-                $stmt -> bindParam( ':idusr', $_SESSION['user_id']);
-                $stmt -> bindParam( ':ncap', $_POST['ncap'] );
-                $stmt -> bindParam( ':npag', $_POST['npag'] );
-                $stmt -> bindParam( ':nlinea', $_POST['nlinea'] ); 
-        
-                $stmt -> execute(); 
-                $results = $stmt->fetch();
-                $msg = 'update';
-                echo $msg;
-                print_r($_POST);
-                echo($results);
-
-            }
-                                   
-    //} 
+    if( !empty( $_POST['tcorrg'] ) ){
+        $sql = "UPDATE gd1 SET tcorrg = :tcorrg,  idusr = :idusr WHERE ncap= :ncap and npag = :npag and nline= :nlinea";
+        $stmt = $conn -> prepare($sql);
+        $stmt -> bindParam( ':tcorrg',  $_POST['tcorrg'] );
+        $stmt -> bindParam( ':idusr', $_SESSION['user_id']);
+        $stmt -> bindParam( ':ncap', $_POST['ncap'] );
+        $stmt -> bindParam( ':npag', $_POST['npag'] );
+        $stmt -> bindParam( ':nlinea', $_POST['nlinea'] ); 
+        $stmt -> execute(); 
+    }                   
 ?>
 <script type = "text/javascript">
             alert("Linea guardada");
