2259d1d4ec9e1fec537de49830ae91020bd831e8
analidia27 <anamechaca27@gmail.com>
2020-08-24 21:58:51 -0300

    Actualiza

--- login-php/corregir.php
+++ login-php/corregir.php
@@ -26,10 +26,39 @@
 
 <body> 
     <?php require 'partials/header.php' ?>
-    <?php if( !empty( $user ) ): ?>
+    
         <div>
         <h1>Revisar Corrección</h1>
-        <?php 
+    <?php 
+    $gd = $_POST['GD'];
+
+    if(isset($_POST['guardar']) ){
+        date_default_timezone_set("America/Argentina/Salta");
+        $fecha = date('Y-m-d H:i:s', time());
+        
+        $sql = "UPDATE gd1 SET tcorrg = :tcorrg, fcorrg = :fcorrg, idusr = :idusr WHERE ncap= :ncap and npag = :npag and nline= :nline";
+        $stmt = $conn -> prepare($sql);
+        $stmt -> bindParam( ':tcorrg',  $_POST['tcorrg'] );
+        $stmt -> bindParam( ':idusr', $_SESSION['user_id']);
+        $stmt -> bindParam( ':ncap', $_POST['ncap'] );
+        $stmt -> bindParam( ':npag', $_POST['npag'] );
+        $stmt -> bindParam( ':nline', $_POST['nline'] ); 
+        $stmt -> bindParam( ':fcorrg', $fecha ); 
+        $stmt -> execute(); 
+        $records = $conn->prepare("SELECT ncap, npag, nline FROM {$gd} WHERE idusr IS NULL LIMIT 1");
+        $records->execute();
+        $results = $records->fetch();
+        $_POST['ncap'] = $results['ncap'];
+        $_POST['npag'] = $results['npag'];
+        $_POST['nline'] = $results['nline'];
+  
+    }
+
+
+/*         if (is_null($_POST['GD'])){
+            echo $_POST['GD'];
+        } */
+
         $records = $conn->prepare("SELECT tocr FROM {$_POST['GD']} WHERE ncap = :ncap AND npag = :npag AND nline = :nline");
         $records->bindParam(':ncap', $_POST['ncap']);
         $records->bindParam(':npag', $_POST['npag']);
@@ -39,19 +68,26 @@
         $ncap = $_POST['ncap'];
         $npag = $_POST['npag'];
         $nline = $_POST['nline'];
-        $tocr = $results[0]; ?>
+        $tocr = $results[0]; 
+        //print_r($_POST); 
+        ?>
         
         
-        <form id = "FORM" method = "POST" action="update.php"> 
+        <form id = "FORM" method = "POST" action=""> 
+            <label for=''>Guemes Documentado: </label>  
+            <input type="text" name ='GD' id ='GD' readonly='true' value=" <?php  echo $_POST['GD']; ?> ">
+            <br>
 
             <label for=''>Capítulo: </label>  
             <input type="text" name ='ncap' id ='ncap' readonly='true' value=" <?php  echo $ncap; ?> ">
             <br>
+
             <label for=''>Página: </label>
             <input type="text" name ='npag' id ='npag' readonly='true' value="<?php  echo  $npag; ?>">
             <br>
+
             <label for=''>Número de línea: </label>
-            <input type="text" name ='nlinea' id ='nlinea' readonly='true' value="<?php  echo $nline; ?>">
+            <input type="text" name ='nline' id ='nline' readonly='true' value="<?php  echo $nline; ?>">
             <br>
 
             <label for=''>Línea original: </label> 
@@ -65,16 +101,16 @@
             <br>
 
             <label for=''>Línea a corregir : </label>
-            <textarea name='tcorrg' id="tcorrg" > </textarea>
+            <p><textarea name='tcorrg' id="tcorrg" required = "true" ></textarea></p>
+            
 
             <br>
-            <input type='submit' name='siguiente' id='siguiente' value='Siguiente'>
+            <input type='submit' name='guardar' id='guardar' value='Guardar'>
          </form>
         <br>
         <a class = b2v href="seleccion.php"> Volver</a>
         <a class = b3v href="logout.php"> Salir</a>
         <br>
-    <?php endif;?>
     
     <script type="text/javascript">
         function copy() {
